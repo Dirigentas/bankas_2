72b318ed4dd824c830e5e1caf2c5b42da4a854fe
Aras <114133924+Dirigentas@users.noreply.github.com>
2023-01-10 12:46:09 +0200

    update

--- view/currencies.php
+++ view/currencies.php
@@ -1,9 +1,15 @@
+<div class="container-sm py-5 px-5" style="text-align:center">
+    <div>
+        <div style="display:flex; justify-content: space-between; flex-direction: column; gap: 10px; border: solid 3px skyblue">
+            <h4 style="display:flex; justify-content: space-evenly; border-bottom: dashed 2px skyblue; flex-wrap: wrap; gap: 5px; padding: 3px" style = "margin-left: 20px"><span></span><span></span>Valiuta<span></span><span>Valiutos kursas už 1 eur</span><span></span></h4>
 
-<!-- <div style="display:flex; justify-content: space-between; flex-direction: column; gap: 10px; border: solid 3px skyblue"> -->
-<h4 style="margin-top: 100px; width 20%; margin-left: 30%; margin-right: 30%; margin top 100 px; padding: 50px; display:flex; justify-content: space-between; border-bottom: dashed 2px skyblue; flex-wrap: wrap; gap: 5px; padding: 3px">Valiuta<span style = "margin-left:100px">Valiutos kursas už 1 eur</span></h4>
-<?php foreach ($currencies['rates'] as $key => $currency) : ?>
-        <h6 style="margin-top: 30px; width 20%; margin-left: 30%; margin-right: 30%; margin top 100 px; padding: 50px; display:flex; justify-content: space-between; border-bottom: dashed 2px skyblue; flex-wrap: wrap; gap: 5px; padding: 3px">
-            <div><?= $key ?> <span style = "margin-left:350px"> <?=$currency?></span></div>
-        </h6>
-    <?php endforeach ?>
+            <?php foreach ($currencies['rates'] as $key => $currency) : ?>
+                <h6 style="display:flex; justify-content: space-evenly; border-bottom: dashed 2px skyblue; flex-wrap: wrap; gap: 5px; padding: 3px">
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-2 col-xl-1"><?= $key ?></div>
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-2 col-xl-1"><?= $currency ?></div>
+                </h6>
+            <?php endforeach ?>
+        </div>
+    </div>
+</div>
 
